ca1c734420a5dc42097270040de29e1d1288e2c2
Santiago Plá <santiago_pla@hotmail.com>
2024-10-28 01:39:32 +0000

    chore: docker working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+    server: {
+        host: true,
+        port: 5173
+    },
+    preview: {
+        host: true,
+        port: 5173
+    }
 })
+
