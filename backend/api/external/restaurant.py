fc061973c3c7a950a54adad6353878604261b60d
Santiago Plá <santiago_pla@hotmail.com>
2024-10-30 01:30:43 +0000

    feat: restaurant endpoint prototypes

--- backend/api/external/restaurant.py
+++ backend/api/external/restaurant.py
@@ -1,3 +1,27 @@
-from fastapi import APIRouter
+from services.restaurant import RestaurantService
+from fastapi import APIRouter, status
+
+from schemas.input_create_restaurant import InputCreateRestaurant
+from schemas.restaurant import Restaurant
 
 router = APIRouter()
+
+@router.post(
+    "/create",
+    status_code=status.HTTP_201_CREATED,
+    response_model=Restaurant
+)
+async def create_restaurant(
+    input_create_restaurant: InputCreateRestaurant
+):
+    return RestaurantService.create(input_create_restaurant)
+
+
+@router.get(
+    "/get/{restaurant_id}",
+    response_model=Restaurant
+)
+async def get_restaurant(
+    restaurant_id: int
+):
+    return restaurant_id
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from external.restaurant import router as restaurant_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-app.include_router(restaurant_router, prefix="/restaurant")
+app.include_router(restaurant_router, prefix="/restaurant", tags=["restaurant"])
