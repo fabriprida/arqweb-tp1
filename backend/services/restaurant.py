97d4795ae50a57bbf29a7e70ffdad198c8b1ac65
Fabrizio Prida <pridafabrizio@gmail.com>
2024-10-30 18:26:00 -0300

    refactor: moved logic inside restaurantService

--- backend/services/restaurant.py
+++ backend/services/restaurant.py
@@ -1,9 +1,22 @@
+from schemas.restaurant import Restaurant
 from schemas.input_create_restaurant import InputCreateRestaurant
 from db.repositories.restaurant import RestaurantRepository
 
 
 class RestaurantService:
    
-   def create_restaurant(input_create_restaurant: InputCreateRestaurant, restaurant_repository: RestaurantRepository):
+    def create_restaurant(input_create_restaurant: InputCreateRestaurant, restaurant_repository: RestaurantRepository):
+        
+        mongo_id = restaurant_repository.create_restaurant(input_create_restaurant)
        
-       return restaurant_repository.create_restaurant(input_create_restaurant)
+        return Restaurant(
+            mongo_id = mongo_id,
+            name = input_create_restaurant.name,
+            latitude = input_create_restaurant.latitude,
+            longitude = input_create_restaurant.longitude,
+            address = input_create_restaurant.address,
+            phone_number = input_create_restaurant.phone_number,
+            email = input_create_restaurant.email,
+            instagram = input_create_restaurant.instagram,
+            timetable = input_create_restaurant.timetable
+        )
