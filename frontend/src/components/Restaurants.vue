37f75a638d7969a5171129771a5fb2838046c555
Santiago Plá <santiago_pla@hotmail.com>
2024-11-01 02:37:19 +0000

    feat: frontend now can communicate with front

---
+++ frontend/src/components/Restaurants.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>My Vue App</h1>
+        <button @click="fetchRestaurantList">Show me the restaurants list</button>
+        <ul v-if="items.length">
+            <li v-for="(item, index) in items" :key="index">{{ item.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            items: []
+        };
+    },
+    methods: {
+        async fetchRestaurantList() {
+            try {
+                const response = await axios.get('http://localhost:8000/internal/restaurant/list');
+                this.items = response.data;
+            } catch (error) {
+                console.error('Error fetching the list:', error);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
