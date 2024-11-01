d0b66a1b81ea513d0dcdcf0f8985780bb97c4731
Santiago Plá <santiago_pla@hotmail.com>
2024-10-31 23:39:05 -0300

    feat/frontend initial config (#6)
    * feat: frontend now can communicate with front
    
    * docs: TODOs: move urls to .env

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.external.restaurant import router as restaurant_router
 
 app = FastAPI()
 
-app.include_router(restaurant_router, prefix="/restaurant", tags=["restaurant"])
+origins = [
+    "http://localhost:5173",   # !TODO: Move to .env
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+app.include_router(restaurant_router, prefix="/internal/restaurant", tags=["restaurant"])
